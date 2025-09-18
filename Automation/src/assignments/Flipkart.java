3726ec632b5e1f4ae94721d47c180dfb5d4758c7
btmadmin <btmadmin@DESKTOP-7HJ645U>
2025-09-18 08:56:48 +0530

    Added a new package for assignments and created a new class Flipkart.java

---
+++ Automation/src/assignments/Flipkart.java
@@ -0,0 +1,19 @@
+package assignments;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Flipkart {
+public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	driver.get("https://flipkart.com");
+	driver.findElement(By.name("q")).sendKeys("iphone14 pro max"+Keys.ENTER);
+	driver.quit();
+}
+}
